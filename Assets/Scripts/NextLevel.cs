aa564c5164eb7112da4cb35e9e86ea7f6fd8fc53
christian-watson <chischun@gmail.com>
2024-04-22 09:19:11 -0500

    Changing levels

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
@@ -9,18 +10,32 @@ public class NextLevel : MonoBehaviour
     public static int doorNum = 1;
 
     private Vector2 startPos = new Vector2(-10,-4);
+    private static string sceneName;
+    private static Scene currentScene;
+
+
+    void Start(){
+        currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+        print(currentScene);
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            print(doorNum);
-            while(doorNum < 5){
+            if(doorNum < 5){
                 Vector2 adjustedPos = new Vector2((80 * doorNum), 0) + startPos;
                 doorNum++;
                 print(adjustedPos);
                 other.gameObject.transform.position = adjustedPos;
-                break;
+            }
+            else{
+                print(sceneName);
+                doorNum = 0;
+                if(sceneName == "Scene_One"){
+                    SceneManager.LoadScene("Scene_Two");
+                }
             }
         }
         
